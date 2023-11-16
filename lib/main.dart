989bbb800a8a2c56ee8fcd80a93ff3743206d4c3
aliaqdas747 <142832510+aliaqdas747@users.noreply.github.com>
2023-11-16 08:23:00 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:e_commerce/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:e_commerce/utils/theme/theme.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //By default the theme will be light theme its depends upon the sysytem them
+      themeMode: ThemeMode.system,
+
+      theme:TAppTheme.lightTheme,
+      //Dark theme
+      darkTheme: TAppTheme.darkTheme,
+    );
+  }
+}
